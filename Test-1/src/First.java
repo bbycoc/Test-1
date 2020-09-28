e9cba32e105e76bf534ffb727ca1e90c5de709d7
bbycoc <bbycoc@qq.com>
2020-09-28 19:35:04 +0800

    test

---
+++ Test-1/src/First.java
@@ -0,0 +1,20 @@
+public class First {
+    private int id;
+    private String name="bby";
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Test-1/src/TE.java
@@ -0,0 +1,6 @@
+public class TE {
+    public static void main(String[] args) {
+        First first=new First();
+        System.out.println(first.getName());
+    }
+}
