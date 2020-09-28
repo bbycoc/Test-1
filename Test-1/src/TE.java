d98fa28e5cf0df7475ac0af9d5ff4217e1b93afe
bbycoc <bbycoc@qq.com>
2020-09-28 20:02:41 +0800

    sadsadas

--- Test-1/src/TE.java
+++ Test-1/src/TE.java
@@ -2,5 +2,8 @@ public class TE {
     public static void main(String[] args) {
         First first=new First();
         System.out.println(first.getName());
+        first.setId(2);
+        first.setName("bby2");
+        System.out.println("id"+"---"+first.getId()+"---"+"name"+"---"+first.getName());
     }
 }
